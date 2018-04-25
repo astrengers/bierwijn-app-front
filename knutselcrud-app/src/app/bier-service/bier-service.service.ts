2800b49000cde7ca6ed4118b84382d0973e572b3
strena <Axel.Strengers@kadaster.nl>
2018-04-25 14:20:24 +0200

    Kleine aanpassingen

--- knutselcrud-app/src/app/bier-service/bier-service.service.ts
+++ knutselcrud-app/src/app/bier-service/bier-service.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {promise} from "selenium-webdriver";
-import {any} from "codelyzer/util/function";
 import {Observable} from "rxjs/Observable";
 
 @Injectable()
@@ -10,7 +8,7 @@ export class BierServiceService {
   constructor(private http: HttpClient) { }
 
  public getAllBier(): Observable<any> {
-    return this.http.get("localhost:8080");
+    return this.http.get("https://localhost:8080");
   }
 
 }
