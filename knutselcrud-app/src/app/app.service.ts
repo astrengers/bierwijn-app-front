bcacbf5e6384542318d6a6d88591d7f82153899d
strena <Axel.Strengers@kadaster.nl>
2018-09-07 15:13:12 +0200

    Headers worden standaard opgeslagen in appservice

--- knutselcrud-app/src/app/app.service.ts
+++ knutselcrud-app/src/app/app.service.ts
@@ -5,17 +5,18 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class AppService {
 
   authenticated = false;
+  headers: HttpHeaders;
 
   constructor(private http: HttpClient) {
   }
 
   authenticate(credentials, callback) {
 
-    const headers = new HttpHeaders(credentials ? {
+    this.headers = new HttpHeaders(credentials ? {
       authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
     } : {});
 
-    this.http.get('http://localhost:8080/user', {headers: headers}).subscribe(response => {
+    this.http.get('http://localhost:8080/user', {headers: this.headers}).subscribe(response => {
       if (response['name']) {
         this.authenticated = true;
       } else {
