9197e1df08ea13e12072b6bdb6bfe1d78d780722
burgtp <Nijmegen02!>
2018-11-02 16:58:27 +0100

    poging tot plaatje toevoegen in formulier

--- knutselcrud-app/src/app/add-bier/add-bier.component.ts
+++ knutselcrud-app/src/app/add-bier/add-bier.component.ts
@@ -16,6 +16,7 @@ export class addBierComponent implements OnInit {
   bier: Bier = new Bier();
   biersoorten = BIERSOORTEN;
   landen: Array<Land>;
+  fileToUpload: File = null;
 
   constructor(private bierService: BierService,
               public activeModal: NgbActiveModal,
@@ -38,4 +39,28 @@ export class addBierComponent implements OnInit {
       this.landen = data;
     })}
 
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+// https://stackoverflow.com/questions/48468957/save-formdata-with-file-upload-in-angular-5
+
+
+
+//   uploadFileToActivity() {
+//     this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+//       this.bier.plaatje = data// do something, if upload success
+//     }, error => {
+//       console.log(error);
+//     });
+//   }
+  // UPLOADSERVICE AANMAKEN //
+  // postFile(fileToUpload: File): Observable<boolean> {
+  //   const endpoint = 'your-destination-url';
+  //   const formData: FormData = new FormData();
+  //   formData.append('fileKey', fileToUpload, fileToUpload.name);
+  //   return this.httpClient
+  //     .post(endpoint, formData, { headers: yourHeadersConfig })
+  //     .map(() => { return true; })
+  //     .catch((e) => this.handleError(e));
+  // }
 }
